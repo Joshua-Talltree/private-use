e08dd85804c681977ba195d2e1896f6693e283a6
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-12-22 14:18:50 -0600

    one more codewars problem down

--- codewars.js
+++ codewars.js
@@ -130,7 +130,7 @@ console.log((maskify('11111'), '#1111'));;
 // Check to see if it's a pangram or not.
 
 function isPangram(string){
-    string = string.toLowerCase();
+    var string = string.toLowerCase();
     return "abcdefghijklmnopqrstuvwxyz"
         .split("").every(function(x){
             return string.indexOf(x) !== -1;
@@ -148,30 +148,36 @@ console.log((isPangram(string), false));
 
 function tribonacci(signature,n) {
     var trib = signature;
-    for (i = 3; i < n; i++) {
+    for (var i = 3; i < n; i++) {
         trib.push((trib[i-1] + trib[i-2] + trib[i-3]));
     }
     return trib.slice(0, n);
 }
 
-Test.describe("Basic tests",function(){
-    console.log(tribonacci([1,1,1],10),[1,1,1,3,5,9,17,31,57,105])
+console.log(tribonacci([1,1,1],10),[1,1,1,3,5,9,17,31,57,105])
+console.log(tribonacci([0,0,1],10),[0,0,1,1,2,4,7,13,24,44])
+console.log(tribonacci([0,1,1],10),[0,1,1,2,4,7,13,24,44,81])
+console.log(tribonacci([1,0,0],10),[1,0,0,1,1,2,4,7,13,24])
+console.log(tribonacci([0,0,0],10),[0,0,0,0,0,0,0,0,0,0])
+console.log(tribonacci([1,2,3],10),[1,2,3,6,11,20,37,68,125,230])
+console.log(tribonacci([3,2,1],10),[3,2,1,6,9,16,31,56,103,190])
+console.log(tribonacci([1,1,1],1),[1])
+console.log(tribonacci([300,200,100],0),[])
+console.log(tribonacci([0.5,0.5,0.5],30),[0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5])
 
-    console.log(tribonacci([0,0,1],10),[0,0,1,1,2,4,7,13,24,44])
 
-    console.log(tribonacci([0,1,1],10),[0,1,1,2,4,7,13,24,44,81])
+// Write a function called validParentheses that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
-    console.log(tribonacci([1,0,0],10),[1,0,0,1,1,2,4,7,13,24])
+function validParentheses(parens){
+    var n = 0;
+    for (var i = 0; i < parens.length; i++) {
+        if (parens[i] == '(') n++;
+        if (parens[i] == ')') n--;
+        if (n < 0) return false;
+    }
 
-    console.log(tribonacci([0,0,0],10),[0,0,0,0,0,0,0,0,0,0])
+    return n == 0;
+}
 
-    console.log(tribonacci([1,2,3],10),[1,2,3,6,11,20,37,68,125,230])
-
-    console.log(tribonacci([3,2,1],10),[3,2,1,6,9,16,31,56,103,190])
-
-    console.log(tribonacci([1,1,1],1),[1])
-
-    console.log(tribonacci([300,200,100],0),[])
-
-    console.log(tribonacci([0.5,0.5,0.5],30),[0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5])
-})
+console.log(validParentheses( "()" ), true);
+console.log(validParentheses( "())" ), false);
